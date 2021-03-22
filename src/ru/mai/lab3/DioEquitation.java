e0a12ca738508daa6b7e86be3d5e08e66a5f1df8
Zaharix <qwertykaj@mail.ru>
2021-03-22 12:26:23 +0300

    added whitespaces

--- src/ru/mai/lab3/DioEquitation.java
+++ src/ru/mai/lab3/DioEquitation.java
@@ -8,7 +8,7 @@ public class DioEquitation {
 
         int arrSize = 0;
 
-        final int Range = 1000;
+        final int range = 1000;
 
         int counter = 0;
 
@@ -20,15 +20,15 @@ public class DioEquitation {
 
         int[] arr = new int[arrSize];
 
-        for(int i = 0; i < arrSize; i++) {
-            if (scan.hasNextInt()){
+        for (int i = 0; i < arrSize; i++) {
+            if (scan.hasNextInt()) {
                 arr[i] = scan.nextInt();
             } else {
                 System.exit(-1);
             }
         }
 
-        for(int i = 1; i <= Range; i++) {
+        for (int i = 1; i <= range; i++) {
             if (checkForSolution(i, arr) == 1) {
                 counter++;
             }
@@ -38,12 +38,13 @@ public class DioEquitation {
     }
 
     public static int checkForSolution(int x, int[] arr) {
+        final int three = 3;
         int a = arr[0];
         int b = arr[1];
         int c = arr[2];
-        int d = arr[3];
+        int d = arr[three];
 
-        if ((Math.pow(x, 3) * a + Math.pow(x, 2) * b + c * x + d) == 0) {
+        if ((Math.pow(x, three) * a + Math.pow(x, 2) * b + c * x + d) == 0) {
             return 1;
         } else {
             return 0;
