f452086714582942b33103b5390a6a68ca80f924
Zaharix <qwertykaj@mail.ru>
2021-03-26 13:45:05 +0300

    sec iteration

--- src/ru/mai/lab3/DioEquitation.java
+++ src/ru/mai/lab3/DioEquitation.java
@@ -5,31 +5,40 @@ import java.util.Scanner;
 public class DioEquitation {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        int arrSize = 0;
-
         final int range = 1000;
 
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int d = 0;
         int counter = 0;
 
         if (scan.hasNextInt()) {
-            arrSize = scan.nextInt();
+            a = scan.nextInt();
         } else {
             System.exit(-1);
         }
 
-        int[] arr = new int[arrSize];
+        if (scan.hasNextInt()) {
+            b = scan.nextInt();
+        } else {
+            System.exit(-1);
+        }
 
-        for (int i = 0; i < arrSize; i++) {
-            if (scan.hasNextInt()) {
-                arr[i] = scan.nextInt();
-            } else {
-                System.exit(-1);
-            }
+        if (scan.hasNextInt()) {
+            c = scan.nextInt();
+        } else {
+            System.exit(-1);
+        }
+
+        if (scan.hasNextInt()) {
+            d = scan.nextInt();
+        } else {
+            System.exit(-1);
         }
 
         for (int i = 1; i <= range; i++) {
-            if (checkForSolution(i, arr) == 1) {
+            if (checkForSolution(i, a, b, c, d) == 1) {
                 counter++;
             }
         }
@@ -37,14 +46,8 @@ public class DioEquitation {
         System.out.println(counter);
     }
 
-    public static int checkForSolution(int x, int[] arr) {
-        final int three = 3;
-        int a = arr[0];
-        int b = arr[1];
-        int c = arr[2];
-        int d = arr[three];
-
-        if ((Math.pow(x, three) * a + Math.pow(x, 2) * b + c * x + d) == 0) {
+    public static int checkForSolution(int x, int a, int b, int c, int d) {
+        if ((Math.pow(x, 3) * a + Math.pow(x, 2) * b + c * x + d) == 0) {
             return 1;
         } else {
             return 0;
