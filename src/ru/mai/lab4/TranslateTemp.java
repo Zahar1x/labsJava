faa782a38e43ac435655160a7d2472fb5aa9dc39
Zaharix <qwertykaj@mail.ru>
2021-03-29 23:54:48 +0300

    First iteration

--- src/ru/mai/lab4/TranslateTemp.java
+++ src/ru/mai/lab4/TranslateTemp.java
@@ -1,16 +1,40 @@
 package ru.mai.lab4;
 
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TranslateTemp {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        String str = scan.nextLine();
+        StringBuilder str = new StringBuilder(scan.nextLine());
 
-        if (str.matches(".+ \\d{1,}F")) {
-            System.out.println(str);
+        final String tempRegex = "\\d{1,}\\.?\\d* ?F";
+
+        Pattern pattern = Pattern.compile(tempRegex);
+        Matcher matcher = pattern.matcher(str);
+
+        if (matcher.find()) {
+            String tempStr = str.substring(matcher.start(), matcher.end() - 1);
+
+            if (tempStr.contains(".")) {
+                printTemperature(tempStr, str, matcher);
+            } else {
+                printTemperature(tempStr, str, matcher);
+            }
         }
+    }
+    public static void printTemperature(String tempStr, StringBuilder str, Matcher matcher) {
+        double tempInF = Double.parseDouble(tempStr.toString());
 
+        double tempInC = (tempInF - 32) * 5 / 9;
+
+        String tempString = String.format(Locale.ROOT, "%.2f", tempInC);
+
+        str.replace(matcher.start(), matcher.end(), tempString + "C");
+
+        System.out.println(str);
     }
 }
