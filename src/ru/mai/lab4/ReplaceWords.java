adf77f392dacadc4e12714d41cfa6ca2846812a2
Zaharix <qwertykaj@mail.ru>
2021-03-29 22:56:07 +0300

    First iteration

--- src/ru/mai/lab4/ReplaceWords.java
+++ src/ru/mai/lab4/ReplaceWords.java
@@ -1,8 +1,7 @@
 package ru.mai.lab4;
 
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 
 public class ReplaceWords {
     public static void main(String[] args) {
