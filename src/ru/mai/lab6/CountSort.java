787f2cb103ef568dbf2748b07ae9bc0893dc033b
Zaharix <qwertykaj@mail.ru>
2021-04-18 19:35:46 +0300

    first

---
+++ src/ru/mai/lab6/CountSort.java
@@ -0,0 +1,41 @@
+package ru.mai.lab6;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class CountSort {
+    static Scanner scan = new Scanner(System.in);
+    static final int MAX = 100;
+    static final int MIN = 0;
+    static final int CAPACITY = 210;
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(CAPACITY);
+
+        while (scan.hasNextInt()) {
+            list.add(scan.nextInt());
+        }
+
+        countSort(list);
+    }
+
+    public static void countSort(ArrayList<Integer> list) {
+        int numCounts[] = new int[MAX + 1];
+        for (int num : list) {
+            numCounts[num]++;
+        }
+
+        int currentSortedIndex = 0;
+        for (int n = 0; n < numCounts.length; n++) {
+            int count = numCounts[n];
+            for (int k = 0; k < count; k++) {
+                list.set(currentSortedIndex, n);
+                currentSortedIndex++;
+            }
+        }
+
+        for (int num : list) {
+            System.out.print(num + " ");
+        }
+    }
+}
