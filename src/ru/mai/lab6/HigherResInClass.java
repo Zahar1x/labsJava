c92f7249ada8ae8056b1d79ab0de1103bc2d8736
Zaharix <qwertykaj@mail.ru>
2021-04-20 15:11:05 +0300

    First try

---
+++ src/ru/mai/lab6/HigherResInClass.java
@@ -0,0 +1,56 @@
+package ru.mai.lab6;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class HigherResInClass {
+    static Scanner scan = new Scanner(System.in);
+    static final int NINE_CLASS = 9;
+    static final int TEN_CLASS = 10;
+    static final int ELEVEN_CLASS = 11;
+    static int higherOfNine = 0;
+    static int higherOfTen = 0;
+    static int higherOfEleven = 0;
+
+    public static void main(String[] args) {
+        int[] higherRes = new int[3];
+        while (scan.hasNext()) {
+            countClass(higherRes);
+        }
+        for (int num : higherRes) {
+            System.out.print(num + " ");
+        }
+    }
+    public static void scanner(ArrayList list) {
+        String str = scan.nextLine();
+        String[] subStr = str.split(" ");
+
+        for (int i = 0; i < subStr.length; i++) {
+            list.add(subStr[i]);
+        }
+    }
+
+    public static void countClass(int[] higherRes) {
+        ArrayList<String> list = new ArrayList<>();
+
+        scanner(list);
+
+        if (Integer.parseInt(list.get(2).toString()) == NINE_CLASS) {
+            if (higherOfNine < Integer.parseInt(list.get(3).toString())) {
+                higherOfNine = Integer.parseInt(list.get(3).toString());
+                higherRes[0] = higherOfNine;
+            }
+
+        } else if (Integer.parseInt(list.get(2).toString()) == TEN_CLASS) {
+            if (higherOfTen < Integer.parseInt(list.get(3).toString())) {
+                higherOfTen = Integer.parseInt(list.get(3).toString());
+                higherRes[1] = higherOfTen;
+            }
+        } else if (Integer.parseInt(list.get(2).toString()) == ELEVEN_CLASS) {
+            if (higherOfEleven < Integer.parseInt(list.get(3).toString())) {
+                higherOfEleven = Integer.parseInt(list.get(3).toString());
+                higherRes[2] = higherOfEleven;
+            }
+        }
+    }
+}
