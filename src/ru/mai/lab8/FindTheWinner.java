d9e89d70b763ac8f9e1ed66a83d29679b45a0d73
Zaharix <qwertykaj@mail.ru>
2021-04-27 16:29:30 +0300

    Next try

--- src/ru/mai/lab8/FindTheWinner.java
+++ src/ru/mai/lab8/FindTheWinner.java
@@ -1,30 +1,82 @@
 package ru.mai.lab8;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.util.*;
 
+/**
+ * Этот класс позволяет найти человека с максимальным кол-вом баллов
+ */
 public class FindTheWinner {
 
-    public static void main(String[] args) {
-        try {
-            InputStreamReader reader = new InputStreamReader(new FileInputStream("input.txt"), StandardCharsets.UTF_8);
-            Scanner scanner = new Scanner(reader);
+    /**
+     * Этот метод считывает данные из файла и отправляет их на обработку
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt"),"Cp1251"));
+             Writer out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("output.txt"), StandardCharsets.UTF_8))) {
+            Scanner scanner = new Scanner(br);
             HashSet<Participant> list = new HashSet<>();
 
             while (scanner.hasNextLine()) {
                 String str = scanner.nextLine();
                 String[] strArr = str.split("\",\"");
-                Participant participant = new Participant(strArr);
-                list.add(participant);
+                if (strArr.length == 2) {
+                    Participant participant = new Participant(strArr);
+                    list.add(participant);
+                } else {
+                    System.out.println("Invalid data!");
+                }
             }
-            System.out.println(list);
+            findWinner(list, out);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-            System.exit(-1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Этот метод находит человека с максимальным кол-вом баллов
+     * @param scores список людей и их баллов
+     * @param out объект, который записывает найденные данные в файл с определенной кодировкой
+     * @throws IOException
+     */
+    public static void findWinner(HashSet<Participant> scores, Writer out) throws IOException {
+        double score = 0.0;
+        double max = 0.0;
+        Participant participantWithMaxScore = null;
+        int counterEqualScores = 0;
+        for (Participant part : scores) {
+            try {
+                String[] scoreOfPart = part.getScore().split(" ");
+                score = Double.parseDouble(scoreOfPart[0].replace(',', '.'));
+                if (score >= max) {
+                    max = score;
+                    participantWithMaxScore = part;
+                }
+            } catch (Exception e) {
+                System.out.println("Can't parse string to double");
+                System.exit(-2);
+            }
+        }
+        for (Participant part : scores) {
+            if (part.getScore() == participantWithMaxScore.getScore()) {
+                counterEqualScores++;
+            }
+        }
+        if (counterEqualScores > 1) {
+            out.write("Победителей не найдено");
+        }
+        else {
+            out.write("\"Победитель-" + participantWithMaxScore.getName()
+                    .replace("\"", "")
+                    + " "
+                    + participantWithMaxScore.getScore());
         }
     }
 }
--- src/ru/mai/lab8/Participant.java
+++ src/ru/mai/lab8/Participant.java
@@ -1,29 +1,49 @@
 package ru.mai.lab8;
 
 /**
- *
+ * Это класс "участник"
  */
 public class Participant {
+
     /**
-     *
+     * Это поле хранит имя участника
      */
     private String name;
 
     /**
-     *
+     * Это поле хранит кол-во баллов участника
      */
     private String score;
 
     /**
-     *
-     * @param nameOf
-     * @param scores
+     * Этот метод является конструктором класса "Участник"
+     * @param arr Массив состоящий из 2-ух элементов. 1-ый имя участника, 2-ой количество баллов участника
      */
     public Participant(String[] arr) {
         this.name = arr[0];
         this.score = arr[1];
     }
 
+    /**
+     * Этот метод позволяет получить имя участника
+     * @return имя участника
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * этот метод позволяет получить кол-во баллов участника
+     * @return кол-во баллов участника
+     */
+    public String getScore() {
+        return score;
+    }
+
+    /**
+     * Этот метод приводит объект класса "участник" к строке и выврдит его имя и кол-во баллов
+     * @return имя и кол-во баллов участника
+     */
     @Override
     public String toString() {
         return this.name + " " + this.score;
